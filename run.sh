1005482393e5e6d6afe0c2de40c391f909d47165
Aminul Huq <41773621+aminul-huq@users.noreply.github.com>
2020-04-17 07:04:43 +0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,13 @@
-python train.py \
-    --model=1 \
-    --lr=0.001 \
-    --itr=5 \
-    --dropout=0.2 \
-    --hidden_dim=256\
-    --device='cuda'\
-    --n_filters=5\
-    --filter_size=1
-    
+#RNN implementation with dropout set to 0 and hidden dimension set to 256
+python train.py --model=0  --lr=0.001 --itr=5 --dropout=0 --hidden_dim=256 --device='cuda' --n_filters=5 --filter_size=1
+#RNN implementation with dropout set to 0.2 and hidden dimension set to 256
+python train.py --model=0  --lr=0.001 --itr=5 --dropout=0.2 --hidden_dim=256 --device='cuda' --n_filters=5 --filter_size=1
+#RNN implementation with dropout set to 0 and hidden dimension set to 512
+python train.py --model=0  --lr=0.001 --itr=5 --dropout=0 --hidden_dim=512 --device='cuda' --n_filters=5 --filter_size=1
+
+#CNN implementation with dropout set to 0 and filter_size/kernel_size set to 1
+python train.py --model=1  --lr=0.001 --itr=5 --dropout=0 --hidden_dim=256 --device='cuda' --n_filters=5 --filter_size=1
+#CNN implementation with dropout set to 0.2 and filter_size/kernel_size set to 1
+python train.py --model=1  --lr=0.001 --itr=5 --dropout=0.2 --hidden_dim=256 --device='cuda' --n_filters=5 --filter_size=1
+#CNN implementation with dropout set to 0.2 and filter_size/kernel_size set to 3
+python train.py --model=1  --lr=0.001 --itr=5 --dropout=0.2 --hidden_dim=256 --device='cuda' --n_filters=5 --filter_size=3
